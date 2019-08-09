fea8c51819395c3e531f5aaec2afbb3e307ae388
ARSHIANJUM <53444406+ARSHIANJUM@users.noreply.github.com>
2019-08-09 06:34:10 -0400

    Add files via upload

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo "Please provide your USERNAME"
+read name
+mkdir $name
+touch $name/$name.cpp
+echo "your folder has been created"
+echo "your file has been created" 
---
+++ q2.sh
@@ -0,0 +1,11 @@
+echo "Please provide your USERNAME"
+read name
+mkdir $name
+touch $name/"$name"1.cpp
+touch $name/"$name"2.cpp
+touch $name/"$name"3.cpp
+touch $name/"$name"4.cpp
+touch $name/"$name"5.cpp
+echo "Your folder has been created"
+echo "Your files have been created"
+
